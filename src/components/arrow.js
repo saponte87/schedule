3800feb8c69cd1a7121f5f5aa6df15e6d175a9bc
Stephen Aponte <saponte1987@gmail.com>
2018-10-10 18:46:48 -0600

    rendering description based on arrow status

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 
 class Arrow extends Component {
 
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-      status: false
-    }
-  }
-
-  toggleArrow = function() {
-    if(this.state.status) {
-        document.getElementById('arrow').classList.remove('arrow-closed');
-    } else {
-        document.getElementById('arrow').classList.add('arrow-closed');
+        this.state = {
+            status: false
+        }
     }
 
-    this.setState({ status: !this.state.status })
-  }.bind(this);
+    toggleArrow = function() {
+        this.props.callback(this.state.status)
+        if(this.state.status) {
+            document.getElementById(this.id).classList.remove('arrow-closed');
+        } else {
+            document.getElementById(this.id).classList.add('arrow-closed');
+        }
 
-  render() {
+        this.setState({ status: !this.state.status })
+    }.bind(this);
+
+    render() {
+        this.id = `arrow-${this.props.id}`
         return (
-            <a id="arrow" onClick={() => this.toggleArrow()}className={`${this.props.className} arrow`}></a>
+            <a id={this.id} onClick={() => this.toggleArrow()}className={`${this.props.className} arrow`}></a>
         )
     }
 }
