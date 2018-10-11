5682b49b9d52e76c462905feed8215df0e0f58b4
Stephen Aponte <saponte1987@gmail.com>
2018-10-11 14:56:19 -0600

    styled schedule course component and refactored schedule grid input rows

--- src/components/action.js
+++ src/components/action.js
@@ -25,16 +25,9 @@ class Action extends Component {
             <a
                 id={this.id}
                 onClick={() => this.handleAction()}
-                className={`${this.props.className} action`}>
-            
+                className={`${this.props.className} action`}>          
             </a>
         )
-        // return (
-        //     <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-        //         { Icon('fas fa-plus-circle') }
-        //         {/* fas fa-times-circle */}
-        //     </button>
-        // )
     }
 }
 
